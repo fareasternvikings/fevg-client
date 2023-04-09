3cba28839d1d07dc1d09004b676fd2eef55b54f1
a.mayatskiy <work.mayatskiy@gmail.com>
2023-04-10 01:14:07 +1000

    fix: lint fix

--- karma.conf.js
+++ karma.conf.js
@@ -25,10 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(
-        __dirname,
-        './coverage/fevg-client'
-      ),
+      dir: require('path').join(__dirname, './coverage/fevg-client'),
       subdir: '.',
       reporters: [{type: 'html'}, {type: 'text-summary'}],
     },
