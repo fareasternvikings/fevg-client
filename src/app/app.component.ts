1ace88bf8a3b6c1a071be0f0719df713562a0969
a.mayatskiy <work.mayatskiy@gmail.com>
2023-04-12 00:38:05 +1000

    feat: add scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {HelloService} from './shared/services/hello.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'fevg-client'
