46d37ff2ccf2b120b941037c33a45ccf76f5e23c
a.mayatskiy <work.mayatskiy@gmail.com>
2023-04-16 19:37:47 +1000

    refactor: return dotenv to old settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL_PROD'],
+  apiUrl: process.env['API_URL_PROD'] || 'Prod url not set',
   // apiUrl: 'hi',
   apiKey: '',
   apiMapKey: '',
