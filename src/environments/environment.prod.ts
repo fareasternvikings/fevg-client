ee4c27c4409e7174bdfe2d41fe39dd36520ece66
a.mayatskiy <work.mayatskiy@gmail.com>
2023-04-16 18:03:58 +1000

    fix: hot fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL_PROD'] || 'Prod url not set',
+  // apiUrl: process.env['API_URL_PROD'] || 'Prod url not set',
+  apiUrl: 'hi',
   apiKey: '',
   apiMapKey: '',
   limit: 10,
