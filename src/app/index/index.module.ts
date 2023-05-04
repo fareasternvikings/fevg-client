d0709c7d6022b8496d8be18da9d715bddc8419a7
a.mayatskiy <work.mayatskiy@gmail.com>
2023-05-04 11:53:01 +1000

    feat: add catalog

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from '@angular/common'
 import {IndexComponent} from './index.component'
 import {IndexRoutingModule} from './index-routing.module'
 import {HeroModule} from './components/hero/hero.module'
+import {CatalogModule} from './components/catalog/catalog.module'
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [CommonModule, IndexRoutingModule, HeroModule],
+  imports: [CommonModule, IndexRoutingModule, HeroModule, CatalogModule],
 })
 export class IndexModule {}
